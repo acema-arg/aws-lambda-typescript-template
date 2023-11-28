f1f9ee3f603f43cf2e1b4a78879afd92cd503263
Carlos <garaycarlosalfredo@gmail.com>
2023-11-28 09:00:56 -0300

    feat: add yup dependency

--- src/functions/example-get/lambda.ts
+++ src/functions/example-get/lambda.ts
@@ -1,10 +1,25 @@
 import { compose } from "ramda";
+import { querySchema, pathSchema, bodySchema } from "./yup/validation";
 import dummyFunction from "./dummy/dummy-function";
 import exampleHandler from "./handler";
 import config from "../../config/config";
 
 const injectToContext = (handler) => (event, context) => {
   return handler(event, { ...context, dummyFunction });
+};
+
+const yupValidation = (handler) => async (event, context) => {
+  // Validate de data with yup
+  const { pathParameters, queryStringParameters } = event;
+
+  try {
+    await querySchema.validate(queryStringParameters);
+    await pathSchema.validate(pathParameters);
+    return handler(event, context);
+  } catch (error) {
+    // (TODO) make better yup validation errors
+    return error;
+  }
 };
 
 /**
@@ -14,7 +29,7 @@ const injectToContext = (handler) => (event, context) => {
  * @returns {Function} An AWS λ handler functions.
  */
 const httpCreateEventHandler = (config) => {
-  return compose(injectToContext)(exampleHandler);
+  return compose(injectToContext, yupValidation)(exampleHandler);
 };
 
 export default httpCreateEventHandler(config);
